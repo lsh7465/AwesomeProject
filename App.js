b63d3723725db42502e9820246f65973cc50ab33
lsh7465 <lsh746527@gmail.com>
2024-03-06 10:57:20 +0900

    react Navigation설치

--- App.js
+++ App.js
@@ -3,15 +3,31 @@ import { StyleSheet, Text, View } from "react-native";
 import MainScreen from "./screens/MainScreen";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "./screens/LoginScreen";
 
 export default function App() {
-  return(
-    <Provider store={store}>
-      <MainScreen />
-    </Provider>
+  const Stack = createNativeStackNavigator();
 
-  )
-    
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Main"
+            component={MainScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
